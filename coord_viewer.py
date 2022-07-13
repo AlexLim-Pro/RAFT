1d54a3f99579bd710a7410dfab5f2d542bcfb379
Alex Lim <alex@virtueware.com>
2022-07-12 21:34:29 -0700

    added ability to cherry-pick from the rivers

--- coord_viewer.py
+++ coord_viewer.py
@@ -1,14 +1,14 @@
 import csv
-import shapefile as shp
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.io import netcdf_file
 import itertools
 
+import matplotlib.pyplot as plt
+import matplotlib.widgets
+import numpy as np
+import shapefile as shp
+from scipy.io import netcdf_file
 
 kfac_scaling = False
 discharge_scaling = True
-discharge_error_scaling = False
 river_length_scaling = False
 
 
@@ -40,8 +40,16 @@ Qout_f_path = "../San_Gaud_data/Qout_San_Guad_exp00.nc"
 default_point_color = "b"
 discharge_graph_rivers = dict()
 river_colors = dict()
+num_downstream = 5
+currently_selected_river = ""
+downstream_rivers_list = list()
+truncated_downstream = False
+select_multiple = False
+select_multiple_label = "Select Multiple"
+selected_rivers_list = list()
 
 cycol = itertools.cycle("bgrcmk")
+
 
 if kfac_scaling:
     i = 0
@@ -87,11 +95,6 @@ with open(coords_f_path, newline="\n") as f:
         elif discharge_scaling:
             idx = np.where(Qout_data_ids == int(row[0]))
             size = Qout_data[0][idx] / Qout_data_max
-            color = default_point_color
-        elif discharge_error_scaling:  # TODO: finish this
-            raise RuntimeError("Not yet implemented")
-            idx = np.where(Qout_data_ids == int(row[0]))
-            size = (Qout_data_err[0][idx] * 1e10) / (Qout_data_err_max * 1e10)
             color = default_point_color
         elif river_length_scaling:
             draw_point = False
@@ -189,10 +192,13 @@ if river_length_scaling:
             ind_id[i] = row[0]
             i += 1
 
+plt.title("Raw River Coordinate Data")
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
 
 Qout_graph_x_vals = list()
 Qout_graph_y_vals = list()
-fig_2 = plt.figure()
+fig_graph = plt.figure()
 for s in range(Qout_data.shape[1]):
     x = list(range(Qout_data.shape[0]))
     y = Qout_data[:, s]
@@ -217,6 +223,7 @@ def on_pick(event):
     """
     Handles selection of points
     """
+    global currently_selected_river
     offsets = event.artist.get_offsets()[event.ind][0]
     i = 0
     for c in coords:
@@ -224,15 +231,201 @@ def on_pick(event):
             break
         i += 1
     id = str(ind_id[i])
+    if select_multiple:
+        print("Selecting river", id)
+        selected_rivers_list.append(id)
+        return
+    currently_selected_river = id
     paths = list()
     done_paths = list()
-    if upstream:
+    if upstream:  # This is no longer supported
         print("Showing upstream for", id)
         for d in connectivity[id][1:]:
             paths.append(str(d))
     else:
         print("Showing downstream for", id)
         paths.append(connectivity[id][0])
+    clear_plots()
+    p[str(id)].set_visible(True)
+    j = 0
+    if str(id) in rivers:
+        for s in rivers[str(id)]:
+            rivers[str(id)][j].set_visible(True)
+            j += 1
+    done_paths.append(str(id))
+    while paths:
+        curr_id = str(paths.pop())
+        if str(curr_id) not in p:
+            continue
+        if upstream:  # This is no longer supported
+            for r in connectivity[str(curr_id)][1:]:
+                paths.append(str(r))
+                downstream_rivers_list.append(str(r))
+        else:
+            paths.append(str(connectivity[str(curr_id)][0]))
+            downstream_rivers_list.append(str(connectivity[str(curr_id)][0]))
+        p[str(curr_id)].set_visible(True)
+        done_paths.append(str(curr_id))
+
+    if upstream:  # This is no longer supported
+        print("Showing upstream for", id)
+        for d in connectivity[id][1:]:
+            paths.append(str(d))
+    else:
+        print("Showing downstream for", id)
+        paths.append(connectivity[id][0])
+    paths = done_paths
+    done_paths = list()
+    while paths:
+        try:
+            curr_id = str(paths.pop())
+            if str(curr_id) not in rivers:
+                continue
+            j = 0
+            for s in rivers[str(curr_id)]:
+                try:
+                    rivers[str(curr_id)][j].set_visible(True)
+                except:
+                    pass
+                j += 1
+            done_paths.append(str(curr_id))
+        except:
+            pass
+
+    if upstream:  # This is no longer supported
+        print("Showing upstream for", id)
+        for d in connectivity[id][1:]:
+            paths.append(str(d))
+    else:
+        print("Showing downstream for", id)
+        paths.append(connectivity[id][0])
+    paths = done_paths
+    done_paths = list()
+    while paths:
+        curr_id = str(paths.pop())
+        if str(curr_id) not in discharge_graph_rivers:
+            continue
+        j = 0
+        for s in discharge_graph_rivers[str(curr_id)]:
+            if str(curr_id) in discharge_graph_rivers:
+                discharge_graph_rivers[str(curr_id)][j].set_visible(True)
+            j += 1
+        done_paths.append(str(curr_id))
+    redraw_canvases()
+
+
+def set_downstream(event):
+    """
+    Sets the option to downstream
+    """
+    global upstream
+    upstream = False
+    print("Set to show downstream")
+
+
+def set_upstream(event):
+    """
+    Sets the option to upstream
+    """
+    global upstream
+    upstream = True
+    print("Set to show upstream")
+
+
+def apply_multiple(event):
+    """
+    Applies the selected rivers
+    """
+    print("Drawing graphs")
+    i = 0
+    for r in selected_rivers_list:
+        fig_temp = plt.figure()
+        plt.title("River " + str(r) + " Discharge Over Time")
+        plt.xlabel("Time (3 hours)")
+        plt.ylabel(r"Average River Discharge ($m^3/s$)")
+        if str(r) in discharge_graph_rivers:
+            j = 0
+            for s in discharge_graph_rivers[str(r)]:
+                r_temp = discharge_graph_rivers[str(r)][j]
+                plt.plot(*r_temp.get_data(),
+                         c=r_temp.get_color(),
+                         linewidth=r_temp.get_linewidth(),
+                         alpha=r_temp.get_alpha())
+                j += 1
+        if i > 10:
+            break
+        i += 1
+    plt.show(block=False)
+
+
+def set_select_multiple(event):
+    """
+    Toggles the select multiple rivers setting
+    """
+    global select_multiple
+    global selected_rivers_list
+    select_multiple = not select_multiple
+    if select_multiple:
+        print("Selecting Multiple")
+    else:
+        print("Selecting Entire Downstream")
+        selected_rivers_list = list()
+
+
+def update_num_downstream(event):
+    """
+    Updates the number of downstream rivers to show
+    """
+    global num_downstream
+    global truncated_downstream
+    if not truncated_downstream:
+        num_downstream = len(downstream_rivers_list)
+        truncated_downstream = True
+    num_downstream_temp = num_downstream_slider.val
+    print(num_downstream_temp)
+    while num_downstream < num_downstream_temp:
+        id = downstream_rivers_list[num_downstream]
+        if str(id) in p:
+            p[str(id)].set_visible(True)
+        if str(id) in rivers:
+            j = 0
+            for s in rivers[str(id)]:
+                rivers[str(id)][j].set_visible(True)
+                j += 1
+        if str(id) in discharge_graph_rivers:
+            j = 0
+            for s in discharge_graph_rivers[str(id)]:
+                discharge_graph_rivers[str(id)][j].set_visible(True)
+                j += 1
+        num_downstream += 1
+    while num_downstream > num_downstream_temp:
+        id = downstream_rivers_list[num_downstream - 1]
+        if str(id) in p:
+            p[str(id)].set_visible(False)
+        if str(id) in rivers:
+            j = 0
+            for s in rivers[str(id)]:
+                rivers[str(id)][j].set_visible(False)
+                j += 1
+        if str(id) in discharge_graph_rivers:
+            j = 0
+            for s in discharge_graph_rivers[str(id)]:
+                discharge_graph_rivers[str(id)][j].set_visible(False)
+                j += 1
+        num_downstream -= 1
+    redraw_canvases()
+
+
+def redraw_canvases():
+    """
+    Redraws all canvases
+    """
+    fig.canvas.draw()
+    fig_graph.canvas.draw()
+    fig_config.canvas.draw()
+
+
+def clear_plots():
     for path in p:
         try:
             p[path].set_visible(False)
@@ -258,90 +451,6 @@ def on_pick(event):
             except:
                 continue
             j += 1
-    p[str(id)].set_visible(True)
-    j = 0
-    if str(id) in rivers:
-        for s in rivers[str(id)]:
-            rivers[str(id)][j].set_visible(True)
-            j += 1
-    done_paths.append(str(id))
-    while paths:
-        curr_id = str(paths.pop())
-        if str(curr_id) not in p:
-            continue
-        if upstream:
-            for r in connectivity[str(curr_id)][1:]:
-                paths.append(str(r))
-        else:
-            paths.append(str(connectivity[str(curr_id)][0]))
-        p[str(curr_id)].set_visible(True)
-        done_paths.append(str(curr_id))
-
-    if upstream:
-        print("Showing upstream for", id)
-        for d in connectivity[id][1:]:
-            paths.append(str(d))
-    else:
-        print("Showing downstream for", id)
-        paths.append(connectivity[id][0])
-    paths = done_paths
-    done_paths = list()
-    while paths:
-        try:
-            curr_id = str(paths.pop())
-            if str(curr_id) not in rivers:
-                continue
-            j = 0
-            for s in rivers[str(curr_id)]:
-                try:
-                    rivers[str(curr_id)][j].set_visible(True)
-                except:
-                    pass
-                j += 1
-            done_paths.append(str(curr_id))
-        except:
-            pass
-
-    if upstream:
-        print("Showing upstream for", id)
-        for d in connectivity[id][1:]:
-            paths.append(str(d))
-    else:
-        print("Showing downstream for", id)
-        paths.append(connectivity[id][0])
-    paths = done_paths
-    done_paths = list()
-    while paths:
-        curr_id = str(paths.pop())
-        if str(curr_id) not in discharge_graph_rivers:
-            continue
-        j = 0
-        for s in discharge_graph_rivers[str(curr_id)]:
-            if str(curr_id) in discharge_graph_rivers:
-                discharge_graph_rivers[str(curr_id)][j].set_visible(True)
-            j += 1
-        done_paths.append(str(curr_id))
-
-    fig.canvas.draw()
-    fig_2.canvas.draw()
-
-
-def set_downstream(event):
-    """
-    Sets the option to downstream
-    """
-    global upstream
-    upstream = False
-    print("Set to show downstream")
-
-
-def set_upstream(event):
-    """
-    Sets the option to upstream
-    """
-    global upstream
-    upstream = True
-    print("Set to show upstream")
 
 
 def reset(event):
@@ -367,28 +476,36 @@ def reset(event):
                 continue
             j += 1
     fig.canvas.draw()
-    fig_2.canvas.draw()
+    fig_graph.canvas.draw()
     print("Finished resetting view")
 
 
 fig.canvas.mpl_connect("pick_event", on_pick)
 
-plt.title("Raw River Coordinate Data")
-plt.xlabel("Longitude")
-plt.ylabel("Latitude")
-
-plt.figure()
+fig_config = plt.figure()
 ax_reset = plt.axes([0.81, 0.05, 0.1, 0.075])
-b_reset = plt.Button(ax_reset, 'Reset')
+b_reset = plt.Button(ax_reset, "Reset")
 b_reset.on_clicked(reset)
-ax_downstream = plt.axes([0.49, 0.05, 0.15, 0.075])
-b_downstream = plt.Button(ax_downstream, 'Downstream')
-b_downstream.on_clicked(set_downstream)
-ax_upstream = plt.axes([0.65, 0.05, 0.15, 0.075])
-b_upstream = plt.Button(ax_upstream, 'Upstream')
-b_upstream.on_clicked(set_upstream)
+ax_apply_multiple = plt.axes([0.65, 0.05, 0.15, 0.075])
+b_apply_multiple = plt.Button(ax_apply_multiple, "Apply")
+b_apply_multiple.on_clicked(apply_multiple)
+ax_select_multiple = plt.axes([0.44, 0.05, 0.2, 0.075])
+b_select_multiple = matplotlib.widgets.CheckButtons(
+    ax_select_multiple, labels=[select_multiple_label])
+b_select_multiple.on_clicked(set_select_multiple)
+ax_num_downstream = plt.axes([0.2, 0.25, 0.0225, 0.63])
+num_downstream_slider = matplotlib.widgets.Slider(
+    ax=ax_num_downstream,
+    label="Number of Downstream Rivers",
+    valmin=0,
+    valmax=10,
+    valinit=num_downstream,
+    valstep=1,
+    orientation="vertical",
+)
+num_downstream_slider.on_changed(update_num_downstream)
 
 print("Finished preprocessing")
 
 
-plt.show()
+plt.show(block=True)
